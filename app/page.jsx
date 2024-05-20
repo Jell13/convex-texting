5f49ea2285c93994322ae9b52ab3fda656f2fe9a
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-05-20 11:24:45 -0700

    made the structure for the convex testing

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <div className="w-screen h-screen">
       <div className="w-full h-full flex justify-center items-center">
-        <h1>Home Page</h1>
+        <div className="flex flex-col">
+          <h1>To-do List</h1>
+          <div className="flex gap-2">
+            <input className="bg-gray-200 px-1 py-1 rounded-md" type="text" />
+            <button className="bg-blue-200 rounded-md px-1">Add</button>
+          </div>
+        </div>
       </div>
     </div>
   );
