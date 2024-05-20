47fc29e197d7c5cee18e0907582be7c2325c6201
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-05-20 16:59:26 -0700

    finished the first mutation

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,19 @@
 "use client"
 
+import { api } from "@convex/_generated/api";
+import { createTodo } from "@convex/todos";
+import { useMutation } from "convex/react";
 import { useState } from "react";
 
 export default function Home() {
 
   const [text, setText] = useState("")
+  const addTodo = useMutation(api.todos.createTodo)
+
+  const handleClick = (text) => {
+    addTodo({text: text})
+    setText("")
+  }
   return (
     <div className="w-screen h-screen">
       <div className="w-full h-full flex justify-center items-center">
@@ -12,7 +21,7 @@ export default function Home() {
           <h1>To-do List</h1>
           <div className="flex gap-2">
             <input value={text} onChange={(e) => setText(e.target.value)} className="bg-gray-200 px-1 py-1 rounded-md" type="text" />
-            <button className="bg-blue-200 rounded-md px-1">Add</button>
+            <button onClick={() => handleClick(text)} className="bg-blue-200 rounded-md px-1">Add</button>
           </div>
         </div>
       </div>
