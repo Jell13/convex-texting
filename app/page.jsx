025de43e45f1103df19b0ce47909a51361a7e180
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-05-21 13:54:28 -0700

    trying out clerk with convex

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { api } from "@convex/_generated/api";
-import { useMutation, useQuery } from "convex/react";
+import { useConvexAuth, useMutation, useQuery } from "convex/react";
 import { useState } from "react";
 import Items from "./_components/Items";
+import { SignInButton } from "@clerk/clerk-react";
 
 export default function Home() {
+
+  const {isAuthenticated} = useConvexAuth()
 
   const [text, setText] = useState("")
   const addTodo = useMutation(api.todos.createTodo)
@@ -21,21 +24,24 @@ export default function Home() {
     deleteTodo({id: id})
   }
   return (
-    <div className="w-screen h-screen">
-      <div className="w-full h-full flex justify-center items-center">
-        <div className="flex flex-col gap-2">
-          <h1>To-do List</h1>
-          <div className="flex gap-2">
-            <input value={text} onChange={(e) => setText(e.target.value)} className="bg-gray-200 px-1 py-1 rounded-md" type="text" />
-            <button onClick={() => handleClick(text)} className="bg-blue-200 rounded-md px-1">Add</button>
-          </div>
-          <div>
-            {todos?.map((todo) => (
-              <Items key={todo._id} id={todo._id} text={todo.text} deleteButton={handleDelete}/>
-            ))}
-          </div>
-        </div>
-      </div>
+    // <div className="w-screen h-screen">
+    //   <div className="w-full h-full flex justify-center items-center">
+    //     <div className="flex flex-col gap-2">
+    //       <h1>To-do List</h1>
+    //       <div className="flex gap-2">
+    //         <input value={text} onChange={(e) => setText(e.target.value)} className="bg-gray-200 px-1 py-1 rounded-md" type="text" />
+    //         <button onClick={() => handleClick(text)} className="bg-blue-200 rounded-md px-1">Add</button>
+    //       </div>
+    //       <div>
+    //         {todos?.map((todo) => (
+    //           <Items key={todo._id} id={todo._id} text={todo.text} deleteButton={handleDelete}/>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+      {isAuthenticated ? "Hello World" : <SignInButton/>}
     </div>
   );
 }
