bce1c9ea0e192dee9a05e9c0c585d4c5c61d0a8d
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-05-23 15:52:54 -0700

    continue on testing

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,7 @@
 
 import { api } from "@convex/_generated/api";
 import { useConvexAuth, useMutation, useQuery } from "convex/react";
-import { useState } from "react";
-import Items from "./_components/Items";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -13,9 +11,7 @@ export default function Home() {
 
   return (
     <div className="w-screen h-screen">
-      <div className="w-full h-full flex justify-center items-center">
-        {isAuthenticated ? <Link href={"/main"}>Main Page</Link> : <SignInButton/>}
-      </div>
+      <UserButton/>
     </div>
   );
 }
